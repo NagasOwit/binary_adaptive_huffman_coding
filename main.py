cb7dadd42f9684993940619abf0690d2c5c8ac69
NagasOwit <61089322+NagasOwit@users.noreply.github.com>
2022-03-08 18:56:03 +0100

    improved naming

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-f = open("book_of_genesis_to_compress.txt", "r").read()
+text_to_compress = open("book_of_genesis_to_compress.txt", "r").read()
 
-res = {}
-  
-for keys in f:
-    res[keys] = res.get(keys, 0) + 1
+symbols_with_propability = {}  
+for keys in text_to_compress:
+    symbols_with_propability[keys] = symbols_with_propability.get(keys, 0) + 1
 
-print ("Count of all characters is :\n " + str(res))
+print ("Count of all characters is :\n " + str(symbols_with_propability))
