b507d270db39c69770080e592b0d59428bae8a84
NagasOwit <61089322+NagasOwit@users.noreply.github.com>
2022-03-08 19:08:26 +0100

    added sorting and easier example to follow

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ class Node:
         self.left_child = left_child
         self.right_child = right_child
 
-text_to_compress = open("book_of_genesis_to_compress.txt", "r").read()
+#text_to_compress = open("book_of_genesis_to_compress.txt", "r").read()
+text_to_compress = "AAABBCCCCCC"
 
 symbols_with_propability = {}  
 for keys in text_to_compress:
     symbols_with_propability[keys] = symbols_with_propability.get(keys, 0) + 1
 
-print ("Count of all characters is :\n " + str(symbols_with_propability))
+sorted_symbols_with_propability = sorted(symbols_with_propability.items(), key=lambda sort: sort[1])
+
+print ("Count of all characters is :\n " + str(sorted_symbols_with_propability))
