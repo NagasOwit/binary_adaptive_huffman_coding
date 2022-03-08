937b3431189a644358366ac01692d99760cbc287
NagasOwit <61089322+NagasOwit@users.noreply.github.com>
2022-03-08 18:51:53 +0100

    dictionary version

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 f = open("book_of_genesis_to_compress.txt", "r").read()
 
-all_freq = {}  
-for i in f:
-    if i in all_freq:
-        all_freq[i] += 1
-    else:
-        all_freq[i] = 1
+res = {}
+  
+for keys in f:
+    res[keys] = res.get(keys, 0) + 1
 
-print ("Count of all characters in GeeksforGeeks is :\n " + str(all_freq))
+print ("Count of all characters is :\n " + str(res))
