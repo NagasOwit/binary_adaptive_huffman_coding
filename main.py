ce7547c7f63577aadeed6cbc127357f5b28384bd
NagasOwit <61089322+NagasOwit@users.noreply.github.com>
2022-03-08 19:22:14 +0100

    symbols as nodes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ class Node:
         self.left_child = left_child
         self.right_child = right_child
 
-def AddToTree():
+def MakeTree(nodes):
     return 0
 
 #text_to_compress = open("book_of_genesis_to_compress.txt", "r").read()
@@ -15,5 +15,4 @@ symbols_with_propability = {}
 for keys in text_to_compress:
     symbols_with_propability[keys] = symbols_with_propability.get(keys, 0) + 1
 
-sorted_symbols_with_propability = sorted(symbols_with_propability.items(), key=lambda sort: sort[1])
-print(str(sorted_symbols_with_propability[0][0]) + " " + str(sorted_symbols_with_propability[0][1]))
+nodes = sorted(symbols_with_propability.items(), key=lambda sort: sort[1])
