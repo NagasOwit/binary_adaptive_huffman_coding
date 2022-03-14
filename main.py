fb65c81ff0a6a76806a069b215d2167a42b9b654
NagasOwit <61089322+NagasOwit@users.noreply.github.com>
2022-03-14 17:57:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,11 @@ class Node:
         self.count = count
         self.index = index
 
-def Recalculate(tree):
+def Recalculate(tree, new_symbol):
+
+    if (new_symbol):
+        
+
     while (tree.value != ""):
         if (tree.left_child.value >= tree.value or tree.right_child.value >= tree.value):
             #switch values
@@ -25,12 +29,16 @@ def Compress(input):
 
     for element in input:
 
-        if (not element in symbol_list): #pokud se jedná o nový symbol, dej jej na místo Epsilon a zakóduj
+        new_symbol = not element in symbol_list
+
+        if (new_symbol): #pokud se jedná o nový symbol, dej jej na místo Epsilon a zakóduj
             symbol_list.append(element)
             compressed_file.write(element)
 
         else: #přiřaď a připočti výskyt do větve
-            tree = Recalculate(tree)
+            compressed_file.write(element)
+        
+        tree = Recalculate(tree, new_symbol)
 
 #text_to_compress = open("book_of_genesis_to_compress.txt", "r").read()
 numbers_to_compress = "0101111100001100100010010000011111001001001001110011111010"
