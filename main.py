40a53fc7b9c859286bae309c3874ec5875e8cb76
NagasOwit <61089322+NagasOwit@users.noreply.github.com>
2022-03-18 18:08:24 +0100

    added code attribute

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 class Node:                  
-    def __init__(self, symbol, parent, left_child, right_child, count, index):
+    def __init__(self, symbol, parent, left_child, right_child, count, index, code):
         self.symbol = symbol
         self.parent = parent
         self.left_child = left_child
         self.right_child = right_child
         self.count = count
         self.index = index
+        self.code = code
 
 class HuffmanTree:
-    escape_symbol = Node("", None, None, None, 0, 1)
+    escape_symbol = Node("", None, None, None, 0, 1, "")
     root = escape_symbol
 
 symbol_list = [] #list of symbols
 tree = HuffmanTree() #Epsilon, počáteční kořen
+tree_list = []
 
 def AddNewSymbolToTree(symbol):
     new_node = tree.escape_symbol
     new_node.symbol = symbol
-    new_node.left_child =  Node(symbol, new_node, None, None, 0, new_node.index + 1)
-    tree.escape_symbol = Node("", new_node, None, None, 0, new_node.index + 2)
+    new_node.left_child =  Node(symbol, new_node, None, None, 0, new_node.index + 1, new_node.code * "0")
+    tree.escape_symbol = Node("", new_node, None, None, 0, new_node.index + 2, new_node.code * "1")
     new_node.right_child =  tree.escape_symbol
     
 
